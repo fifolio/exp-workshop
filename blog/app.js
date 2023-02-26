6148f11d7b73fd8c5ec69fcee4a6abe002f040c0
Firas <firasdabbabi@gmail.com>
2023-02-26 12:35:32 +0100

    version 1.6

---
+++ blog/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(port, () => {
+  console.log(`we are connected at port ${port}`);
+});
