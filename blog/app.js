ca0619594e670bedd0810b081fc0ba78dc516307
Firas <firasdabbabi@gmail.com>
2023-02-26 18:02:35 +0100

    version 1.7

--- blog/app.js
+++ blog/app.js
@@ -7,23 +7,17 @@ app.use(bodyParser.json());
 // CONTROLLERS
 const controller = require("./controllers/posts");
 
-// DATA
-const posts = require("./models/data");
-
-// MIDDLEWARES
-const mid1 = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
-
-// PASSING MIDDLEWARE TO ALL ROUTES
-app.use(mid1);
-
 app.get("/posts", controller.getAll);
 app.get("/posts/:id", controller.getOne);
 app.delete("/posts/:id", controller.deleteOne);
 app.patch("/posts/:id", controller.updateOne);
 app.post("/posts/", controller.addOne);
+
+const errMiddleware = (err, req, res, next) => {
+  res.statue(400).json({ err: err });
+};
+
+app.use(errMiddleware);
 
 app.listen(port, () => {
   console.log(`we are connected at port ${port}`);
