15220d46870fadd10e043b04538b382214ea7a15
Firas <firasdabbabi@gmail.com>
2023-02-26 20:07:20 +0100

    version 1.8

--- blog/app.js
+++ blog/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 const postsRouter = require("./routes/posts");
 
 app.use(bodyParser.json());
 app.use("/posts", postsRouter);
 
 const errMiddleware = (err, req, res, next) => {
-  res.statue(400).json({ err: err });
+  res.status(400).json({ err: err });
 };
 app.use(errMiddleware);
 
--- blog/routes/posts.js
+++ blog/routes/posts.js
@@ -1,11 +1,29 @@
+// EXPRESS
 const express = require("express");
+const app = express;
 
 // CONTROLLERS
 const controller = require("../controllers/posts");
 
-const route = express.Router();
+// ROUTES
+const route = app.Router();
+
+const validateId = (req, res, next) => {
+  const id = req.params.id;
+  const pattern = /^[1-9]{1,3}$/
+  if (pattern.test(id)) {
+    next();
+  } else {
+    next({
+      name: "validation Error",
+      element: "params: id",
+      msg: "The post id must be a number from 1-999",
+    });
+  }
+};
+
 route.get("/", controller.getAll);
-route.get("/:id", controller.getOne);
+route.get("/:id", [validateId], controller.getOne);
 route.delete("/:id", controller.deleteOne);
 route.patch("/:id", controller.updateOne);
 route.post("/", controller.addOne);
